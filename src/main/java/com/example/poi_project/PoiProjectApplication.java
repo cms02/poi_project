8da40aa92396e452594fd6d70b522f53852a2c46
cms02 <minoui1234@gmail.com>
2023-06-01 14:39:58 +0900

    first commit

---
+++ src/main/java/com/example/poi_project/PoiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.poi_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/poi_project/PoiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poi_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoiProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
